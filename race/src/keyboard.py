37005668e3255972e756d4969e8a825912b76152
khc196 <khc146@gmail.com>
2018-03-21 21:05:41 +0900

    Update keyboard.py

--- race/src/keyboard.py
+++ race/src/keyboard.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import Bool
-from std_msgs.msg import UInt16
+from race.msg import drive_values
 import curses
 import time
 
@@ -11,23 +10,40 @@ stdscr = curses.initscr()
 curses.cbreak()
 stdscr.keypad(1)
 rospy.init_node('keyboard_talker', anonymous=True)
-pub = rospy.Publisher('throttle', Bool, queue_size=1)
-pub2 = rospy.Publisher('Controller', UInt16, queue_size=1)
+pub = rospy.Publisher('Controller', drive_values, queue_size=1)
 
 stdscr.refresh()
 key = ''
-start = False
+throttle = 0
+steering = 0
 
 while key != ord('q'):
 	key = stdscr.getch()
 	stdscr.refresh()
 	if key == curses.KEY_UP: 
-		stdscr.addstr(2, 20, "start")
-		start = True
+		stdscr.addstr(2, 20, "Accel")
+		throttle += 1
+		if throttle > 1 :
+			throttle = 1
 	elif key == curses.KEY_DOWN :
-		stdscr.addstr(2, 20, "stop!")
-		start = False
-	msg = Bool()
-	msg.data = start
+		stdscr.addstr(2, 20, "Brake")
+		throttle -= 1
+		if throttle < -1 :
+			throttle = -1
+	elif key == curses.KEY_LEFT :
+		steering -= 10
+		if steering < -100 : 
+			steering = -100
+	elif key == curses.KEY_RIGHT :
+		steering += 10
+		if steering > 100 :
+			steering = 100
+	elif key == curses.KEY_DC :
+		steering = 0
+	if throttle == 0 :
+		stdscr.addstr(2, 20, "Stop!")
+	msg = drive_values()
+	msg.throttle = throttle
+	msg.steering = steering + 100
 	pub.publish(msg)
 curses.endwin()
