d5b614c55c2a6d80cd134cdc8c00a65ba245ae6f
khc196 <khc146@gmail.com>
2018-03-24 19:55:53 +0900

    Update

--- race/src/Lane_Detector.cpp
+++ race/src/Lane_Detector.cpp
@@ -11,46 +11,6 @@ const CvScalar COLOR_RED = CvScalar(0, 0, 255);
 
 queue <int> buffer_x;
 
-class Lane_Detector {
-private :
-    Point p1, p2, p3, p4;
-    float p_slope;
-    float p_position;
-    float left_slope;
-    float right_slope;
-    float left_length;
-    float right_length;
-    VideoCapture capture_left;
-    VideoCapture capture_right;
-    Mat originImg_left;
-    Mat originImg_right;
-    bool left_error;
-    bool right_error;
-    Mat grayImg1, grayImg2, otsu, sobelX_Img, sobelY_Img, sobel_Img1, sobel_Img2,
-        imageROI1, imageROI2, blured1, blured2, mask, openingImg1, openingImg2,
-        cannyImg1, cannyImg2, houghImg1, houghImg2;
-    void v_roi(Mat& img, Mat& img_ROI, const Point& p1, const Point& p2);
-    void region_of_interest_L(Mat& img, Mat& img_ROI);
-    void region_of_interest_R(Mat& img, Mat& img_ROI);
-    bool hough_left(Mat& img, Point* p1, Point* p2);
-    bool hough_right(Mat& img, Point* p1, Point* p2);
-    bool get_intersectpoint(const Point& AP1, const Point& AP2,
-                           const Point& BP1, const Point& BP2, Point* IP);
-    float get_slope(const Point& p1, const Point& p2);
-    int position(const Point P1, const Point P2);
-public :
-    Lane_Detector(){}
-    void init();
-    void operate();
-    void set_control_variables(float p_slope, float p_position);
-	float get_p_position();
-	float get_p_slope();
-    float get_left_slope();
-    float get_right_slope();
-    float get_left_length();
-    float get_right_length();
-};
-
 float Lane_Detector::get_p_position() {
 	return p_position;
 }
