5d1133d5f6c495fa01e2dc37c8869b933afdc275
khc196 <khc146@gmail.com>
2018-03-24 21:58:30 +0900

    Update Structure

--- race/src/main.cpp
+++ race/src/main.cpp
@@ -1,14 +1,26 @@
-#include "Controller.h"
+#include "Lane_Detector.h"
 
+Lane_Detector* ld;
+race::drive_values control_msg;
+ros::NodeHandle nh;
+ros::Subscriber sub; 
+ros::Publisher control_pub;
+
+void testerCallback(const race::control_variables &msg) {
+    ld->set_control_variables(msg.p_slope, msg.p_position);
+}
+void generate_control_msg(race::drive_values* control_msg) {
+
+}
 int main(int argc, char** argv) {
     ros::init(argc, argv, "Controller");
-    ros::NodeHandle nh;
-    Controller* controller = new Controller();
-    ros::Subscriber sub = nh.subscribe("control_variables", 1000, controller->testerCallback);
-    ros::Publisher control_pub = nh.advertise<race::drive_values>("Control", 1000);
-    race::drive_values control_msg;
+    ld = new Lane_Detector();
+    sub = nh.subscribe("control_variables", 1000, testerCallback);
+    control_pub = nh.advertise<race::drive_values>("Control", 1000);
+    
     while(true) {
-        controller->generate_control_msg(&control_msg);
+        ld->operate();
+        generate_control_msg(&control_msg);
         control_pub.publish(control_msg);
         ros::spinOnce();
     }
