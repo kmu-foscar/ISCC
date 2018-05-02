3a1ed08a3dbad35feb46336ddf0cd1b0e87b2388
khc196 <khc146@gmail.com>
2018-05-02 21:42:21 +0900

    Lookahead version 1

--- race/include/Look_Ahead.hpp
+++ race/include/Look_Ahead.hpp
@@ -12,287 +12,111 @@
 #include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
-
+#include "Lane_Detector.hpp"
 
 using namespace cv;
 using namespace std;
 
-const CvScalar COLOR_BLUE = CvScalar(255, 0, 0);
-const CvScalar COLOR_RED = CvScalar(0, 0, 255);
-
-class Look_Ahead {
+class Look_Ahead : public Lane_Detector{
 private :
-    float left_slope;
-    float right_slope;
-    float left_length;
-    float right_length;
-    VideoCapture capture;
-    Mat originImg, originImg_left, originImg_right;
-    bool left_error;
-    bool right_error;
-    Mat grayImg, filterImg1, filterImg2, otsu, sobelX_Img, sobelY_Img, sobel_Img1, sobel_Img2,
-        imageROI1, imageROI2, bluredImg, blured1, blured2, mask, openingImg1, openingImg2,
-        cannyImg1, cannyImg2, houghImg1, houghImg2;
-    VideoWriter outputVideo;
-
-    void v_roi(Mat& img, Mat& img_ROI, const Point& p1, const Point& p2);
-    void region_of_interest_L(Mat& img, Mat& img_ROI);
-    void region_of_interest_R(Mat& img, Mat& img_ROI);
-    bool hough_left(Mat& img, Point* p1, Point* p2);
-    bool hough_right(Mat& img, Point* p1, Point* p2);
-    float get_slope(const Point& p1, const Point& p2);
-    int position(const Point P1, const Point P2);
+  Mat croppedImg1, croppedImg2;
+  bool hough_left(Mat& img, Point* p1, Point* p2);
+  bool hough_right(Mat& img, Point* p1, Point* p2);
+  void v_roi(Mat& img, Mat& img_ROI, const Point& p1, const Point& p2);
+  void region_of_interest_L(Mat& img, Mat& img_ROI);
+  void region_of_interest_R(Mat& img, Mat& img_ROI);
+   
 public :
-    Point p1, p2, p3, p4;
-    Look_Ahead(){}
-    void init();
-    void operate();
-    float get_left_slope();
-    float get_right_slope();
-    float get_left_length();
-    float get_right_length();
-    bool is_left_error();
-    bool is_right_error();
-    bool get_intersectpoint(const Point& AP1, const Point& AP2,
-                           const Point& BP1, const Point& BP2, Point* IP);
+  Look_Ahead();  
+  void operate(Mat originImg_left, Mat originImg_right);
 };
-
-bool Look_Ahead::is_left_error() {
-  return left_error;
-}
-bool Look_Ahead::is_right_error() {
-  return right_error;
-}
-int Look_Ahead::position(const Point P1, const Point P3) {
-    float x_L;
-    float x_R;
-    const float y = 480;
-
-    x_L = (y - P1.y + left_slope * P1.x) / left_slope;
-    left_length = 640 - x_L;
-
-    x_R = (y - P3.y + right_slope * P3.x) / right_slope;
-    right_length = x_R;
-}
-
-float Look_Ahead::get_left_length() {
-    return left_length;
-}
-
-float Look_Ahead::get_right_length() {
-    return right_length;
-}
-
-float Look_Ahead::get_left_slope() {
-    return left_slope;
-}
-float Look_Ahead::get_right_slope() {
-    return right_slope;
-}
-
-void Look_Ahead::init(){
-  capture = VideoCapture("/home/hwancheol/Downloads/ISCC_sample_.avi");
-  outputVideo.open("ouput.avi", VideoWriter::fourcc('X', 'V', 'I', 'D'), 30, Size(640, 60), true);
-
+Look_Ahead::Look_Ahead() {
   mask = getStructuringElement(MORPH_RECT, Size(3, 3), Point(1, 1));
-
   left_error = false;
   right_error = false;
   left_length = 0;
   right_length = 0;
+  output_video.open("lookahead.avi", VideoWriter::fourcc('X', 'V', 'I', 'D'), 20, Size(1280, 480), true);
 }
+void Look_Ahead::operate(Mat originImg_left_LD, Mat originImg_right_LD) {
+  originImg_left_LD.copyTo(originImg_left);
+  originImg_right_LD.copyTo(originImg_right);
+  croppedImg1 = originImg_left(Rect(200, 0, 440, 40));
+  croppedImg2 = originImg_right(Rect(0, 0, 440, 40));
+  GaussianBlur(croppedImg1, filterImg1, Size(3, 3), 0);
+	GaussianBlur(croppedImg2, filterImg2, Size(3, 3), 0);
 
-void Look_Ahead::operate(){
-    capture >> originImg;
+	cvtColor(filterImg1, img_hsv, COLOR_BGR2HSV);
 
-    if (originImg.empty()){
-      cerr << "Empty Image" << endl;
-      return;
-    }
-    //imshow("original", originImg);
-    originImg_left = originImg(Rect(320, 0, 320, 60));
-    originImg_right = originImg(Rect(640, 0, 320, 60));
-    Mat Img_copy_L = originImg_left;
-		Mat Img_copy_R = originImg_right;
-    Mat imgHSV1, imgThresholded1;
-    cvtColor(Img_copy_L, imgHSV1, CV_BGR2HSV);
-    inRange(imgHSV1, Scalar(10, 20, 50), Scalar(30, 200, 255), imgThresholded1); 
-    bitwise_and(Img_copy_L, Img_copy_L, mask = imgThresholded1);
-		GaussianBlur(Img_copy_L, filterImg1, Size(3, 3), 0);
-		GaussianBlur(Img_copy_R, filterImg2, Size(5, 5), 0);
-    Canny(filterImg1, cannyImg1, (filterImg1.rows + filterImg1.cols) / 4, (filterImg1.rows + filterImg1.cols) / 2) ;
-    Canny(filterImg2, cannyImg2, (filterImg2.rows + filterImg2.cols) / 4, (filterImg2.rows + filterImg2.cols) / 2) ;
-		imshow("canny1", cannyImg1);
-		imshow("canny2", cannyImg2);
+	inRange(img_hsv, HSV_YELLOW_LOWER, HSV_YELLOW_UPPER, binaryImg1);
+	inRange(croppedImg2, RGB_WHITE_LOWER, RGB_WHITE_UPPER, binaryImg2);
 
-    if(!left_error){
-      v_roi(cannyImg1, imageROI1, p1, p2);
-    }
-    else{
-      region_of_interest_L(cannyImg1, imageROI1);
-    }
-    if(!right_error){
-      v_roi(cannyImg2, imageROI2, p3, p4);
-    }
-    else{
-      region_of_interest_R(cannyImg2, imageROI2);
-    }
+	Canny(binaryImg1, cannyImg1, (binaryImg1.rows + binaryImg1.cols)/4, (binaryImg1.rows + binaryImg1.cols)/2);
+	Canny(binaryImg2, cannyImg2, (binaryImg2.rows + binaryImg2.cols)/4, (binaryImg2.rows + binaryImg2.cols)/2);
 
-    left_error = hough_left(cannyImg1, &p1, &p2);
-    right_error = hough_right(cannyImg2, &p3, &p4);
+	Mat initROI1;
+	Mat initROI2;
 
-    line(Img_copy_L, p1, p2, COLOR_RED, 4, CV_AA);
-    line(Img_copy_R, p3, p4, COLOR_RED, 4, CV_AA);
+	region_of_interest_L(croppedImg1, initROI1);
+	region_of_interest_R(croppedImg2, initROI2);
 
-    cout << p1.x << " " << p1.y << " " << p2.x << " " << p2.y << endl;
+	if(!left_error){
+		Mat ddd;
+		v_roi(cannyImg1, initROI1, p1, p2);
+	}
+	else{
+		region_of_interest_L(cannyImg1, initROI1);
+	}
 
-    left_slope = get_slope(p1, p2);
-    right_slope = get_slope(p3, p4);
-    position(p1, p3);
-		Mat a;
-		Mat b;
-		Mat c;
-		resize(Img_copy_L, a, Size(320, 60), 0, 0, CV_INTER_LINEAR);
-		resize(Img_copy_R, b, Size(320, 60), 0, 0, CV_INTER_LINEAR);
-		hconcat(a, b, c);
-    outputVideo << c;
-    imshow("result", c);
-    if(waitKey(10) == 0){
-      return;
-    }
+	if(!right_error){
+    v_roi(cannyImg2, initROI2, p3, p4);
+	}
+	else{
+		region_of_interest_R(cannyImg2, initROI2);
+	}
+
+	left_error = hough_left(initROI1, &p1, &p2);
+	right_error = hough_right(initROI2, &p3, &p4);
+  Point p1_copy = p1;
+  p1_copy.x += 200;
+  Point p2_copy = p2;
+  p2_copy.x += 200;
+	line(originImg_left, p1_copy, p2_copy, COLOR_BLUE, 4, CV_AA);
+	line(originImg_right, p3, p4, COLOR_BLUE, 4, CV_AA);
+
+	cout << p1.x << " " << p1.y << " " << p2.x << " " << p2.y << endl;
+
+	left_slope = get_slope(p1, p2);
+	right_slope = get_slope(p3, p4);
+	position(p1, p3);
+
+	Mat a;
+	Mat b;
+	Mat c;
+
+	resize(originImg_left, a, Size(640, 480), 0, 0, CV_INTER_LINEAR);
+	resize(originImg_right, b, Size(640, 480), 0, 0, CV_INTER_LINEAR);
+	hconcat(a, b, c);
+
+	imshow("lookahead", c);
+
+	output_video << c;
+	if(waitKey(10) == 0){
+		return;
+	}
 }
-
-void Look_Ahead::v_roi(Mat& img, Mat& img_ROI, const Point& p1, const Point& p2){
-
-
-  float slope = get_slope(p1, p2);
-  float alphaY = 50.f / sqrt(slope*slope + 1);
-  float alphaX = slope * alphaY;
-
-  Point a(p1.x - alphaX, p1.y + alphaY );
-  Point b(p1.x + alphaX, p1.y - alphaY );
-  Point c(p2.x + alphaX, p2.y - alphaY );
-  Point d(p2.x - alphaX, p2.y + alphaY );
-
-  vector <Point> Left_Point;
-
-  Left_Point.push_back(a);
-  Left_Point.push_back(b);
-  Left_Point.push_back(c);
-  Left_Point.push_back(d);
-
-  Mat roi(img.rows, img.cols, CV_8U, Scalar(0));
-
-  fillConvexPoly(roi, Left_Point, Scalar(255));
-
-  Mat filteredImg_Left;
-  img.copyTo(filteredImg_Left, roi);
-
-
-  img_ROI = filteredImg_Left.clone();
-
-  // imshow("V_ROI", img_ROI);
-}
-
-
-void Look_Ahead::region_of_interest_L(Mat& img, Mat& img_ROI){
-  Point a = Point(0, img.rows/2);
-  Point b = Point(img.cols, 0);
-  Point c = Point(img.cols, img.rows);
-  Point d = Point(0, img.rows);
-
-  vector <Point> Left_Point;
-
-  Left_Point.push_back(a);
-  Left_Point.push_back(b);
-  Left_Point.push_back(c);
-  Left_Point.push_back(d);
-
-
-  Mat roi(img.rows, img.cols, CV_8U, Scalar(0));
-
-  fillConvexPoly(roi, Left_Point, Scalar(255));
-
-  Mat filteredImg_Left;
-  img.copyTo(filteredImg_Left, roi);
-
-  img_ROI = filteredImg_Left.clone();
-
-  // imshow("Img_ROI", img_ROI);
-}
-
-void Look_Ahead::region_of_interest_R(Mat& img, Mat& img_ROI){
-  Point a = Point(img.cols, img.rows);
-  Point b = Point(img.cols, img.rows/2);
-  Point c = Point(0, 0);
-  Point d = Point(0, img.rows);
-
-  vector <Point> Left_Point;
-
-  Left_Point.push_back(a);
-  Left_Point.push_back(b);
-  Left_Point.push_back(c);
-  Left_Point.push_back(d);
-
-
-  Mat roi(img.rows, img.cols, CV_8U, Scalar(0));
-
-  fillConvexPoly(roi, Left_Point, Scalar(255));
-
-  Mat filteredImg_Left;
-  img.copyTo(filteredImg_Left, roi);
-
-
-  img_ROI = filteredImg_Left.clone();
-
-  Left_Point.push_back(c);
-  // imshow("Img_ROIa", Img_ROI);
-}
-
-float Look_Ahead::get_slope(const Point& p1, const Point& p2){
-
-  float slope;
-
-  slope = ((float) p2.y - (float) p1.y) / ((float) p2.x - (float) p1.x);
-
-  return slope;
-}
-
-bool Look_Ahead::get_intersectpoint(const Point& AP1, const Point& AP2,
-                       const Point& BP1, const Point& BP2, Point* IP)
-{
-    double t;
-    double s;
-    double under = (BP2.y-BP1.y)*(AP2.x-AP1.x)-(BP2.x-BP1.x)*(AP2.y-AP1.y);
-    if(under==0) return false;
-
-    double _t = (BP2.x-BP1.x)*(AP1.y-BP1.y) - (BP2.y-BP1.y)*(AP1.x-BP1.x);
-    double _s = (AP2.x-AP1.x)*(AP1.y-BP1.y) - (AP2.y-AP1.y)*(AP1.x-BP1.x);
-
-    t = _t/under;
-    s = _s/under;
-
-    if(t<0.0 || t>1.0 || s<0.0 || s>1.0) return false;
-    if(_t==0 && _s==0) return false;
-
-    IP->x = AP1.x + t * (double)(AP2.x-AP1.x);
-    IP->y = AP1.y + t * (double)(AP2.y-AP1.y);
-
-    return true;
-}
-
 bool Look_Ahead::hough_left(Mat& img, Point* p1, Point* p2){
 
   vector<Vec2f> linesL;
 
-  Point point1;
-  Point point2;
-
   int count = 0, x1 = 0, x2 = 0, y1 = 0, y2 = 0;
-  int threshold = 60;
+  int threshold = 20;
 
   for (int i = 10; i > 0; i--){
     HoughLines(img, linesL, 1, CV_PI / 180, threshold, 0, 0, 0, CV_PI /2);
+    int clusterCount = 2;
+    Mat h_points = Mat(linesL.size(), 1, CV_32FC2);
+    Mat labels, centers;
     if (linesL.size() > 1){
       for (size_t i = 0; i < linesL.size(); i++){
         count ++;
@@ -300,40 +124,61 @@ bool Look_Ahead::hough_left(Mat& img, Point* p1, Point* p2){
         float theta = linesL[i][1];
         double a = cos(theta), b = sin(theta);
         double x0 = a * rho, y0 = b * rho;
+        // cout << "x0, y0 : " << rho << ' ' << theta << endl;
+        h_points.at<Point2f>(i, 0) = Point2f(rho, (float)(theta*100));
+      }
+      kmeans(h_points, clusterCount, labels,
+            TermCriteria(TermCriteria::COUNT+TermCriteria::EPS, 10, 1.0),
+               3, KMEANS_RANDOM_CENTERS, centers);
+
+      Point mypt1 = centers.at<Point2f>(0,0);
+
+      float rho = mypt1.x;
+      float theta = (float)mypt1.y/100;
+      double a = cos(theta), b = sin(theta);
+      double x0 = a * rho, y0 = b * rho;
+
+      // cout << "pt : " << mypt1.x << ' ' << mypt1.y << endl;
 
         int _x1 = int(x0 + 1000*(-b));
         int _y1 = int(y0 + 1000*(a));
         int _x2 = int(x0 - 1000*(-b));
         int _y2 = int(y0 - 1000*(a));
 
-        point1.x = _x1; point1.y = _y1;
-        point2.x = _x2; point2.y = _y2;
+        x1 += _x1;
+        y1 += _y1;
 
+        x2 += _x2;
+        y2 += _y2;
 
-        x1 += point1.x;
-        y1 += point1.y;
+        Point mypt2 = centers.at<Point2f>(1,0);
 
-        x2 += point2.x;
-        y2 += point2.y;
+      rho = mypt2.x;
+      theta = (float)mypt2.y/100;
+      a = cos(theta), b = sin(theta);
+      x0 = a * rho, y0 = b * rho;
 
-      }
+      // cout << "pt : " << mypt2.x << ' ' << mypt2.y << endl;
+
+        _x1 = int(x0 + 1000*(-b));
+        _y1 = int(y0 + 1000*(a));
+        _x2 = int(x0 - 1000*(-b));
+        _y2 = int(y0 - 1000*(a));
+
+        x1 += _x1;
+        y1 += _y1;
+
+        x2 += _x2;
+        y2 += _y2;
+
       break;
-    }
+    };
   }
-
   if (count != 0){
-
-    x1 /= count;
-    y1 /= count;
-
-    x2 /= count;
-    y2 /= count;
-
-    p1->x = x1; p1->y = y1;
-    p2->x = x2; p2->y = y2;
+    p1->x = x1/2; p1->y = y1/2;
+    p2->x = x2/2; p2->y = y2/2;
 
     return false;
-
   }
   return true;
 }
@@ -341,14 +186,14 @@ bool Look_Ahead::hough_left(Mat& img, Point* p1, Point* p2){
 bool Look_Ahead::hough_right(Mat& img, Point* p1, Point* p2){
   vector<Vec2f> linesR;
 
-  Point point1;
-  Point point2;
-
   int count = 0, x1 = 0, x2 = 0, y1 = 0, y2 = 0;
-  int threshold = 60;
+  int threshold = 50;
 
   for (int i = 10; i > 0; i--){
     HoughLines(img, linesR, 1, CV_PI / 180, threshold, 0, 0, CV_PI/2, CV_PI);
+    int clusterCount = 2;
+    Mat h_points = Mat(linesR.size(), 1, CV_32FC2);
+    Mat labels, centers;
     if (linesR.size() > 1){
       for (size_t i = 0; i < linesR.size(); i++){
         count ++;
@@ -356,39 +201,143 @@ bool Look_Ahead::hough_right(Mat& img, Point* p1, Point* p2){
         float theta = linesR[i][1];
         double a = cos(theta), b = sin(theta);
         double x0 = a * rho, y0 = b * rho;
+        // cout << "x0, y0 : " << rho << ' ' << theta << endl;
+        h_points.at<Point2f>(i, 0) = Point2f(rho, (float)(theta*100));
+      }
+      kmeans(h_points, clusterCount, labels,
+            TermCriteria(TermCriteria::COUNT+TermCriteria::EPS, 10, 1.0),
+               3, KMEANS_RANDOM_CENTERS, centers);
+
+      Point mypt1 = centers.at<Point2f>(0,0);
+
+      float rho = mypt1.x;
+      float theta = (float)mypt1.y/100;
+      double a = cos(theta), b = sin(theta);
+      double x0 = a * rho, y0 = b * rho;
+
+      // cout << "pt : " << mypt1.x << ' ' << mypt1.y << endl;
 
         int _x1 = int(x0 + 1000*(-b));
         int _y1 = int(y0 + 1000*(a));
         int _x2 = int(x0 - 1000*(-b));
         int _y2 = int(y0 - 1000*(a));
 
-        point1.x = _x1; point1.y = _y1;
-        point2.x = _x2; point2.y = _y2;
+        x1 += _x1;
+        y1 += _y1;
 
+        x2 += _x2;
+        y2 += _y2;
 
-        x1 += point1.x;
-        y1 += point1.y;
+        Point mypt2 = centers.at<Point2f>(1,0);
 
-        x2 += point2.x;
-        y2 += point2.y;
+      rho = mypt2.x;
+      theta = (float)mypt2.y/100;
+      a = cos(theta), b = sin(theta);
+      x0 = a * rho, y0 = b * rho;
 
-      }
+      // cout << "pt : " << mypt2.x << ' ' << mypt2.y << endl;
+
+        _x1 = int(x0 + 1000*(-b));
+        _y1 = int(y0 + 1000*(a));
+        _x2 = int(x0 - 1000*(-b));
+        _y2 = int(y0 - 1000*(a));
+
+        x1 += _x1;
+        y1 += _y1;
+
+        x2 += _x2;
+        y2 += _y2;
+
       break;
     };
   }
   if (count != 0){
-    x1 /= count;
-    y1 /= count;
-
-    x2 /= count;
-    y2 /= count;
-
-    p1->x = x1; p1->y = y1;
-    p2->x = x2; p2->y = y2;
+    p1->x = x1/2; p1->y = y1/2;
+    p2->x = x2/2; p2->y = y2/2;
 
     return false;
   }
   return true;
+}
+void Look_Ahead::v_roi(Mat& img, Mat& img_ROI, const Point& p1, const Point& p2){
+
+
+  float slope = get_slope(p1, p2);
+  float alphaY = 80.f / sqrt(slope*slope + 1);
+  float alphaX = slope * alphaY;
+
+	Point a(p1.x - alphaX, p1.y + alphaY );
+  Point b(p1.x + alphaX, p1.y - alphaY );
+  Point c(p2.x, p2.y);
+
+  vector <Point> Left_Point;
+
+  Left_Point.push_back(a);
+  Left_Point.push_back(b);
+  Left_Point.push_back(c);
+
+  Mat roi(img.rows, img.cols, CV_8U, Scalar(0));
+
+  fillConvexPoly(roi, Left_Point, Scalar(255));
+
+  Mat filteredImg_Left;
+  img.copyTo(filteredImg_Left, roi);
+
+
+  img_ROI = filteredImg_Left.clone();
+
+}
+
+void Look_Ahead::region_of_interest_L(Mat& img, Mat& img_ROI){
+  Point a = Point(0, 0);
+  Point b = Point(0, img.rows);
+  Point c = Point(img.cols, img.rows);
+  Point d = Point(img.cols, 0);
+
+  vector <Point> Left_Point;
+
+  Left_Point.push_back(a);
+  Left_Point.push_back(b);
+  Left_Point.push_back(c);
+  Left_Point.push_back(d);
+
+
+  Mat roi(img.rows, img.cols, CV_8U, Scalar(0));
+
+  fillConvexPoly(roi, Left_Point, Scalar(255));
+
+  Mat filteredImg_Left;
+  img.copyTo(filteredImg_Left, roi);
+
+  img_ROI = filteredImg_Left.clone();
+
+}
+
+void Look_Ahead::region_of_interest_R(Mat& img, Mat& img_ROI){
+	Point a = Point(0, 0);
+	Point b = Point(0, img.rows);
+	Point c = Point(img.cols, img.rows);
+	Point d = Point(img.cols, 0);
+
+  vector <Point> Left_Point;
+
+  Left_Point.push_back(a);
+  Left_Point.push_back(b);
+  Left_Point.push_back(c);
+  Left_Point.push_back(d);
+
+
+  Mat roi(img.rows, img.cols, CV_8U, Scalar(0));
+
+  fillConvexPoly(roi, Left_Point, Scalar(255));
+
+  Mat filteredImg_Left;
+  img.copyTo(filteredImg_Left, roi);
+
+
+  img_ROI = filteredImg_Left.clone();
+
+  Left_Point.push_back(c);
 }
 
 
