2d81ac022dbfbc147749ac627c1b08b985d08d7b
hyerim <hyerim9543@naver.com>
2018-05-15 01:10:25 +0900

    urg system remove

--- race/src/controller_vision.cpp
+++ race/src/controller_vision.cpp
@@ -168,16 +168,6 @@ void cw_onoffCallback(const std_msgs::Bool &msg) {
     cw_onoff = msg.data;
 }
 void do_onoffCallback(const std_msgs::Bool &msg) {
-    if(do_onoff != msg.data) {
-	if(do_onoff) {
-	    system("rosnode kill connect_to_urg_node");
-	    system("rosrun urg_node urg_node _ip_address:=192.168.0.10 _angle_min:=-1.57 _angle_max:=1.57");
-	}
-	else {
-	    system("rosnode kill connect_to_urg_node");
-	    system("rosrun urg_node urg_node _ip_address:=192.168.0.10 _angle_min:=-0.29 _angle_max:=0.29");
-	}
-    }
     do_onoff = msg.data;
 }
 void so_onoffCallback(const std_msgs::Bool &msg) {
