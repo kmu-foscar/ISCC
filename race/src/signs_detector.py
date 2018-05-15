9899332a295f208625b61456e19496a3e0444f9b
khc196 <khc146@gmail.com>
2018-05-16 03:13:04 +0900

    two model

--- race/src/signs_detector.py
+++ race/src/signs_detector.py
@@ -22,7 +22,60 @@ cam = cv2.VideoCapture()
 cnt = 0
 step = 0
 
-def crop_sign(image):
+def crop_sign_red(image):
+    cv_image = image
+    (rows, cols, channels) = cv_image.shape
+
+    #imCrop2 = cv_image[0:240,400:640]
+    imCrop2 = cv_image
+
+    hsv = cv2.cvtColor(imCrop2, cv2.COLOR_BGR2HSV)
+    lower_red = np.array([0, 50, 0])
+    upper_red = np.array([10, 255, 255])
+    lower_red2 = np.array([160, 50, 0])
+    upper_red2 = np.array([180, 255, 255])
+    mask_r1 = cv2.inRange(hsv, lower_red, upper_red)
+    mask_r2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    mask_ = mask_r1 + mask_r2
+
+    image, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+																																	
+    if (len(contours) > 0):
+        max_area = 0
+        ci = 0
+
+        for i in range(len(contours)):
+            cnt = contours[i]
+            area = cv2.contourArea(cnt)
+
+            if (area > max_area):
+                max_area = area
+                ci = i
+
+        cnt = contours[ci]
+
+        epsilon = 0.1 * cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, epsilon, True)
+        x, y, w, h = cv2.boundingRect(approx)
+
+        if w > 30 and h > 30 and w / h >= 0.4 and w / h <= 2.0:
+            for i in range(0, 7) :
+                mask = cv2.dilate(mask, np.ones((int(w/10), int(h/10)), np.uint8), iterations=1)
+
+            for i in range(0, 7) :
+                mask = cv2.erode(mask, np.ones((int(w/10), int(h/10)), np.uint8), iterations=1)
+
+            imCrop2 = cv2.bitwise_and(imCrop2, imCrop2, mask=mask)
+
+            a = y
+            b = y + h
+            c = x
+            d = x + w
+            imCrop = imCrop2[a:b, c:d]
+
+            return imCrop
+
+def crop_sign_blue(image):
     cv_image = image
     (rows, cols, channels) = cv_image.shape
 
@@ -32,15 +85,8 @@ def crop_sign(image):
     hsv = cv2.cvtColor(imCrop2, cv2.COLOR_BGR2HSV)
     lower_blue = np.array([90, 70, 50])
     upper_blue = np.array([120, 255, 255])
-    lower_red = np.array([0, 50, 0])
-    upper_red = np.array([10, 255, 255])
-    lower_red2 = np.array([160, 50, 0])
-    upper_red2 = np.array([180, 255, 255])
     mask_b = cv2.inRange(hsv, lower_blue, upper_blue)
-    mask_r1 = cv2.inRange(hsv, lower_red, upper_red)
-    mask_r2 = cv2.inRange(hsv, lower_red2, upper_red2)
-    mask_r = mask_r1 + mask_r2
-    mask = mask_r + mask_b
+    mask = mask_b
 
     image, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 																																	
@@ -106,65 +152,79 @@ if __name__ == '__main__' :
 	rate = rospy.Rate(10)
 	cam.open(0)
 	try :
-		with tf.gfile.FastGFile("work/signs/retrained_graph_v2.pb", 'rb') as f:
-    			graph_def = tf.GraphDef()		
-    			graph_def.ParseFromString(f.read())
-    			_ = tf.import_graph_def(graph_def, name='')
+		with tf.gfile.FastGFile("work/signs/retrained_graph_red.pb", 'rb') as f:
+    		graph_def_red = tf.GraphDef()		
+    		graph_def_red.ParseFromString(f.read())
+    		_ = tf.import_graph_def(graph_def_red, name='graph_red')
+
+		with tf.gfile.FastGFile("work/signs/retrained_graph_blue.pb", 'rb') as f:
+    		graph_def_blue = tf.GraphDef()		
+    		graph_def_blue.ParseFromString(f.read())
+    		_2 = tf.import_graph_def(graph_def_blue, name='graph_blue')
 
 		with tf.Session() as sess:
-			label_lines = [line.rstrip() for line in tf.gfile.GFile("work/signs/retrained_labels.txt")]
-			softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-    			
+			label_lines_red = [line.rstrip() for line in tf.gfile.GFile("work/signs/retrained_labels_red.txt")]
+			label_lines_blue = [line.rstrip() for line in tf.gfile.GFile("work/signs/retrained_labels_blue.txt")]
+			softmax_tensor_red = sess.graph.get_tensor_by_name('final_result_red:0')
+			softmax_tensor_blue= sess.graph.get_tensor_by_name('final_result_blue:0')
+			
+
 			while True:
 				if(not sc_onoff or not cam.isOpened()) :
 					continue
-        			ret_val, img = cam.read()
-        			img = cv2.flip(img, 0)
-        			img = cv2.flip(img, 1)
+				red_flag = True
+        		ret_val, img = cam.read()
+        		img = cv2.flip(img, 0)
+        		img = cv2.flip(img, 1)
 
 				cv2.imshow("origin", img)
-        			img = img[50:300, 150:550]
+        		img = img[50:300, 150:550]
 				
-        			img_crop = crop_sign(img)
+        		img_crop = crop_sign_red(img)
 				
-        			if img_crop is None :
-					img_crop = img	
-				cv2.imshow("ROI", img)
+        		if img_crop is None :
+					img_crop = crop_sign_blue(img)
+					red_flag = False
+				
+				if img_crop is None :
+					img_crop = img
 
-        			if ret_val:
-            				predictions = sess.run(softmax_tensor, {'DecodeJpeg:0': img})
+				cv2.imshow("ROI", img_crop)
 
-            				top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]			
-            				max_score = 0
+        		if ret_val:
+					if(red_flag) :
+            			predictions = sess.run(softmax_tensor_red, {'DecodeJpeg:0': img})
+					else :
+						predictions = sess.run(softmax_tensor_blue, {'DecodeJpeg:0': img})
+						
+            		top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]			
+            		max_score = 0
 
-            				prediction = ""
+            		prediction = ""
 
 					for node_id in top_k:
-                				human_string = label_lines[node_id]
-                				score = predictions[0][node_id]
-
-						#if score < 0.4:
-						#	continue
+                		human_string = label_lines[node_id]
+                		score = predictions[0][node_id]
 						
-			        	        if score > max_score :
+			        	if score > max_score :
 							prediction = human_string
-					                max_score = score  
-					if not prediction == 'no sign' :
+					        max_score = score  
+
+					if not prediction == 'no sign' and max_score > 0.5 :
 						buff[step % 15] = prediction 
-	    					step = step + 1
+	    				step = step + 1
 
-	    				count = 0
-					print(prediction)
-	    				for i in range(0, 15) :
+	    			count = 0
+	    			for i in range(0, 15) :
 						if buff[i] == prediction :
-							count = count + 1	
+						count = count + 1	
 
-					if count >= 3 :
+					if count >= 10 :
 						print('%s (score = %.5f)' % (prediction, max_score))
 						talker(prediction)
 						for i in range(0, 15) :
 							buff[i] = ""
-            				print()
+            			print()
 				if cv2.waitKey(1) & 0xFF == ord('q'):
 					break
 				rate.sleep()
